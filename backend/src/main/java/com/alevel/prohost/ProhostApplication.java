37f954038bbf5c0d5a25409fc9acc1364a112b23
irina <pravotorovairina12@gmail.com>
2018-12-18 19:22:38 +0200

    backend: initial commit

---
+++ backend/src/main/java/com/alevel/prohost/ProhostApplication.java
@@ -0,0 +1,14 @@
+package com.alevel.prohost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProhostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProhostApplication.class, args);
+    }
+
+}
+
